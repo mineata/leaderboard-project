607d51fb7a785453dfc4dda311fff7d7ac646577
matalay <matalay@sabanciuniv.edu>
2020-12-19 22:22:18 +0300

    initializer extended from main app

--- src/main/java/com/gamescore/GameScoreApplication.java
+++ src/main/java/com/gamescore/GameScoreApplication.java
@@ -3,12 +3,19 @@ package com.gamescore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@EnableCaching
-public class GameScoreApplication {
+public class GameScoreApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GameScoreApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GameScoreApplication.class, args);
