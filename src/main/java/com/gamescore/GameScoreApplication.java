379c4a4494127d44e03a9adc57216a47315311b8
matalay <matalay@sabanciuniv.edu>
2020-12-20 13:21:52 +0300

    flexible

--- src/main/java/com/gamescore/GameScoreApplication.java
+++ src/main/java/com/gamescore/GameScoreApplication.java
@@ -3,19 +3,12 @@ package com.gamescore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@EnableCaching
-public class GameScoreApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(GameScoreApplication.class);
-    }
+public class GameScoreApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(GameScoreApplication.class, args);
